868baf6fa3bc2e3d271ea110ea75b9f1af9610cd
thoenselaar <t.a.h.hoenselaar@student.tue.nl>
2017-11-23 17:50:58 +0100

    wow, finished the first puzzle of day 6

---
+++ day6/puzzle1/puzzle1.py
@@ -0,0 +1,35 @@
+with open("input.txt") as f:
+    content = f.readlines()
+content = [x.strip() for x in content]
+
+lstLength = len(content[0])
+lst = [[] for _ in range(lstLength)]
+
+for i in range(len(content)):
+	for j in range(8):
+		lst[j].append(content[i][j])
+
+lst2 = lst[0]
+for i in range(8):
+	lst[i] = sorted(lst[i])
+
+def countChars(lijst):
+	uniqueChars = []
+	countList = []
+	for i in range(len(lijst)):
+		if lijst[i] not in uniqueChars:
+			uniqueChars.append(lijst[i])
+			countList.append(lijst.count(lijst[i]))
+	return uniqueChars, countList
+
+def maxChar(uniqueCharacters, countedChars):
+	maxIndex = countedChars.index(max(countedChars))
+	return uniqueCharacters[maxIndex]
+
+code = ''
+for k in range(lstLength):
+	uniqueChars, countedChars = countChars(lst[k])
+	maxCharacter = maxChar(uniqueChars,countedChars)
+	code += maxCharacter
+
+print(code)
