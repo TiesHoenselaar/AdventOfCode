422249eda4c1b378d6cec1a6358620e966f6287d
TiesHoenselaar <t.hoenselaar@gisarts.nl>
2017-11-24 13:37:16 +0100

    finished the second puzzle, yaygit add .

--- day7/puzzle2/puzzle2.py
+++ day7/puzzle2/puzzle2.py
@@ -1,42 +1,38 @@
-testString = "abba[mnop]qrst"
-
-def testABA(string):
-	ABA = 0
+def testABA(string, BAB):
 	for i in range(len(string)-2):
 		if string[i] == string[i+2] and string[i] != string[i+1]:
-			ABA = 1
+			BAB.append(string[i+1]+string[i]+string[i+1])
 		i += 1
-	return ABA
+	return BAB
 
-def testBAB(string):
-	BAB = 0
+def testBAB(string, BAB):
 	for i in range(len(string)-2):
 		if string[i] == string[i+2] and string[i] != string[i+1]:
-			BAB = 1
+			BAB.append(string[i]+string[i+1]+string[i+2])
 		i += 1
-	return ABA
-
-def testTSL(IP):
-	if (testABBA(IP[0]) or testABBA(IP[2])) and not testABBA(IP[1]):
-		return 1
-	else:
-		return 0
+	return BAB
 
 with open("input.txt") as f:
     content = f.readlines()
 content = [x.strip().replace('[',',').replace(']',',').split(',') for x in content]
 
-count = 0
+globalCount = 0
 for j in range(len(content)):
-	sum1 = 0
-	sum2 = 0
+	BABs = []
+	BABs2 = []
+	count = 0
 	for k in range(len(content[j])):
 		if k%2 == 0:
-			sum1 += testABBA(content[j][k])
+			BABs = testABA(content[j][k],BABs)
 		elif k%2 == 1:
-			sum2 += testABBA(content[j][k])
+			BABs2 = testBAB(content[j][k],BABs2)
 	
-	if sum1 >= 1 and sum2 == 0:
-		count += 1
+	for i in range(len(BABs2)):
+		if BABs2[i] in BABs:
+			count += 1
 
-print(count)
+	if count > 0:
+		globalCount += 1
+
+print(globalCount)
+
