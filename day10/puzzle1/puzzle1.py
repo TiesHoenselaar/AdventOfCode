01c86b60052ec125313472708f7fae4ef4a6de57
thoenselaar <t.a.h.hoenselaar@student.tue.nl>
2017-11-28 22:50:47 +0100

    finished the first and second puzzle of day 10 at once

--- day10/puzzle1/puzzle1.py
+++ day10/puzzle1/puzzle1.py
@@ -1,3 +1,4 @@
+# with open("testInput.txt") as f:
 with open("input.txt") as f:
     content = f.readlines()
 content = [x.strip() for x in content]
@@ -21,9 +22,59 @@ for i in range(len(content)):
 	if botNumber(content[i]) > maxBotNumber:
 		maxBotNumber = botNumber(content[i])
 
-print(maxBotNumber) ### 209
+# print(maxBotNumber) ### 209
 
 botChoices = []
-
-for i in range(maxBotNumber):
+storage = []
+for i in range(maxBotNumber+1):
 	botChoices.append([-1, -1])
+	storage.append([])
+
+## initialize botChoices and storage
+for i in range(len(content)):
+	stringSplitted = content[i].split(' ')
+	if stringSplitted[0] == 'bot':
+		if stringSplitted[5] == 'bot':
+			botChoices[int(stringSplitted[1])][0] = int(stringSplitted[6])
+		elif stringSplitted[5] == 'output':
+			botChoices[int(stringSplitted[1])][0] = -1-int(stringSplitted[6])
+
+		if stringSplitted[10] == 'bot':
+			botChoices[int(stringSplitted[1])][1] = int(stringSplitted[11])
+		elif stringSplitted[10] == 'output':
+			botChoices[int(stringSplitted[1])][1] = -1-int(stringSplitted[11])
+	elif stringSplitted[0] == 'value':
+		storage[int(stringSplitted[5])].append(int(stringSplitted[1]))
+
+output = []
+numOutputs = -min(min(botChoices))
+for j in range(numOutputs):
+	output.append([])
+
+def loopOnce():
+	for bot in range(len(storage)):
+		if len(storage[bot]) > 1:
+			if storage[bot] == [61, 17] or storage[bot] == [17, 61]:
+				print('puzzle 1:', bot)
+			minValue = min(storage[bot])
+			maxValue = max(storage[bot])
+
+			if botChoices[bot][0] < 0:
+				output[-botChoices[bot][0]-1].append(minValue)
+			else:
+				storage[botChoices[bot][0]].append(minValue)
+
+			if botChoices[bot][1] < 0:
+				output[-botChoices[bot][1]-1].append(maxValue)
+			else:
+				storage[botChoices[bot][1]].append(maxValue)
+
+			storage[bot] = []
+
+
+
+
+while max(storage) != []:
+	loopOnce()
+
+print('puzzle 2:', output[0][0]*output[1][0]*output[2][0])
