e3f1e0df5015d375912b10ee162d45d5628b4321
TiesHoenselaar <t.hoenselaar@gisarts.nl>
2017-12-07 13:54:34 +0100

    started with day 6

---
+++ 2017/day6/puzzle1.py
@@ -0,0 +1,8 @@
+# memoryBank = [0, 5, 10, 0, 11, 14, 13, 4, 11, 8, 8, 7, 1, 4, 12, 11]
+memoryBank = [0, 2, 7, 0]
+
+memoryBanks = []
+memoryBanks.append(memoryBank)
+memoryBanks.append(memoryBank)
+
+print(memoryBanks)
