5f00da498bf630fb5ed4ce0e5fea41ff6388536c
TiesHoenselaar <tieshoenselaar@gmail.com>
2022-12-06 23:14:54 +0100

    finished day 1

---
+++ 2022/day1/puzzle2/puzzle2.py
@@ -0,0 +1,19 @@
+input_file = "puzzle2/input.txt"
+
+
+total_calories = 0
+current_total = 0
+elfs_calories = []
+with open(input_file, "r") as input_file:
+    for line in input_file:
+        stripped_line = line.strip()
+        if stripped_line == "":
+            elfs_calories.append(current_total)
+            current_total = 0
+        else:
+            current_total += int(stripped_line)
+
+
+elfs_calories.sort(reverse=True)
+
+print(sum(elfs_calories[:3]))
